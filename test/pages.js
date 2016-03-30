9c0fab4fc018b07d81dfc2a17fd90cba13ff65c8
Arjen <aratramba@gmail.com>
2016-03-30 11:58:46 +0200

    remove tests

--- test/pages.js
+++ test/pages.js
@@ -1,41 +1,11 @@
 'use strict';
-/* global require, __dirname */
+/* global require */
 
 var test = require('tape');
 var isThere = require('is-there');
 var rimraf = require('rimraf');
-var mkdirp = require('mkdirp');
-
-const DIR = __dirname +'/tmp/';
-const PAGES_DIR = __dirname +'/fixtures/pages/';
 
 
-/**
- * check if a page is being generated
- */
-
-rimraf(DIR, function(){
-  mkdirp.sync(DIR);
-
-  test('pages', function(assert){
-    assert.plan(1);
-
-    var options = {
-      output: DIR,
-      meta: {
-        domain: 'website.com',
-        title: 'Style Guide',
-        avatar: 'http://placehold.it/80x80'
-      }
-    };
-
-    var pages = require('../lib/pages');
-    pages.createPage(PAGES_DIR +'Index.md', 'Home', {}, options, [], false, function(){
-
-      var actual = isThere(__dirname +'/tmp/home.html');
-      var expected = true;
-      assert.equal(actual, expected, 'page should be generated');
-      assert.end();
-    });
-  });
+test('pages', function(assert){
+  assert.fail();
 });
