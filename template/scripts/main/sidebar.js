35ff5eff98bcdce45b8075359f4e6947a807a77c
Arjen <aratramba@gmail.com>
2017-06-07 16:08:06 +0200

    add scrollto with offset for location hash #74

--- template/scripts/main/sidebar.js
+++ template/scripts/main/sidebar.js
@@ -13,23 +13,47 @@ var $navLinks = document.querySelectorAll('.js-sidebar__item');
  * Internal link click handler
  */
 
-
-function onLinkClick(e) {
-  var href = e.delegateTarget.getAttribute('href');
-  if (href && document.querySelector(href)) {
-    e.preventDefault();
-    var top = document.querySelector(href).getBoundingClientRect().top;
+function scrollWithOffset(href) {
+  var $el = document.querySelector(href);
+  if ($el) {
+    var top = $el.getBoundingClientRect().top;
     var $header = document.querySelector('.header');
     if ($header) {
       top -= $header.getBoundingClientRect().height;
     }
     window.scrollBy(0, top - 20);
+  }
+}
+
+
+function onLinkClick(e) {
+  var href = e.delegateTarget.getAttribute('href');
+
+  // check if it's an anchor link
+  if (href && document.querySelector(href)) {
+
+    // stop scroll
+    e.preventDefault();
+
+    // scroll to element
+    scrollWithOffset(href)
+
+    // replace history item
     history.replaceState(null, null, href);
+
+    // close offcanvas just in case
     document.getElementById('hamburger').checked = false;
   }
 }
 
 delegate.bind(document.body, 'a', 'click', onLinkClick);
+
+if (location.hash) {
+  console.log(location.hash);
+  setTimeout(function() {
+    scrollWithOffset(location.hash)
+  }, 0);
+}
 
 
 
