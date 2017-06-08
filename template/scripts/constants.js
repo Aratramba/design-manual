049fcb79a53f4490937a9eb5c77a6af033678f50
Arjen <aratramba@gmail.com>
2017-06-08 08:39:29 +0200

    clean up js

--- template/scripts/constants.js
+++ template/scripts/constants.js
@@ -5,7 +5,6 @@ var constants = {
   EVENT_SECTION_INVIEW: 'section-in-view',
   LOADING_CLASS: 'is-loading',
   ACTIVE_CLASS: 'is-active',
-  SIDEBAR_EMPTY_CLASS: 'sidebar-is-empty',
   RESIZING_CLASS: 'is-resizing'
 };
 
