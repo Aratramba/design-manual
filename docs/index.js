6ce5876a0d725fc54eb6f942925791f7afad3a9f
Arjen <aratramba@gmail.com>
2017-06-13 08:21:41 +0200

    add docs folder

---
+++ docs/index.js
@@ -0,0 +1,25 @@
+'use strict';
+/* global require */
+
+var rimraf = require('rimraf');
+rimraf.sync('./*.html');
+
+var DesignManual = require('../index');
+
+new DesignManual({
+  forceUpdate: true,
+  output: 'docs/',
+  pages: 'docs/pages/',
+  components: 'examples/components.json',
+  meta: {
+    domain: 'github.com/eightmedia/design-manual',
+    title: 'Design Manual',
+    avatar: '/assets/avatar.png',
+    version: 'v' + require('../package.json').version
+  },
+  headHtml: '',
+  bodyHtml: '',
+  brandColor: '#EB1D38',
+  brandColorContrast: 'white',
+  prerenderComponents: false
+});
