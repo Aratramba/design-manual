5c70c1b155df535ecdca7a6c84e3d308abbe4b2f
Arjen <aratramba@gmail.com>
2017-08-25 21:34:01 +0200

    refactor generator: now uses forked child process instead of direct function calls

--- examples/example.js
+++ examples/example.js
@@ -4,52 +4,60 @@
 var rimraf = require('rimraf');
 rimraf.sync('httpdocs/*');
 
-var DesignManual = require('../index');
+var dm = require('../lib/index');
 
-new DesignManual({
-  output: 'examples/httpdocs/',
-  pages: 'examples/pages/',
-  components: 'examples/components.json',
-  meta: {
-    domain: 'website.com',
-    title: 'Design Manual',
-    avatar: 'http://placehold.it/100x50',
-    version: 'v' + require('../package.json').version
-  },
-  nav: [
-    { label: 'Home', href: '/index.html' },
-    { label: 'Components', href: '/components.html' },
-    { label: 'Content Guidelines', href: '/content-guidelines.html' },
-    { label: 'Design Principles', href: '/design-principles.html' }
-  ],
-  headHtml: '',
-  bodyHtml: '',
-  contentsFlag: 'contents',
-  componentHeadHtml: `
-    <link rel="stylesheet" href="/lib.css">
-  `,
-  componentBodyHtml: `
-  <script>
-    var $hamburger = document.querySelector('.hamburger');
-    if ($hamburger) {
-      $hamburger.addEventListener('click', function(e) {
-        document.body.classList.toggle('nav-is-open');
-      });
+function build() {
+  dm.build({
+    output: 'examples/httpdocs/',
+    pages: 'examples/pages/',
+    components: 'examples/components.json',
+    meta: {
+      domain: 'website.com',
+      title: 'Design Manual',
+      avatar: 'http://placehold.it/100x50',
+      version: 'v' + require('../package.json').version
+    },
+    nav: [
+      { label: 'Home', href: '/index.html' },
+      { label: 'Components', href: '/components.html' },
+      { label: 'Content Guidelines', href: '/content-guidelines.html' },
+      { label: 'Design Principles', href: '/design-principles.html' }
+    ],
+    headHtml: '',
+    bodyHtml: '',
+    contentsFlag: 'contents',
+    componentHeadHtml: `
+      <link rel="stylesheet" href="/lib.css">
+    `,
+    componentBodyHtml: `
+    <script>
+      var $hamburger = document.querySelector('.hamburger');
+      if ($hamburger) {
+        $hamburger.addEventListener('click', function(e) {
+          document.body.classList.toggle('nav-is-open');
+        });
+      }
+    </script>
+    `,
+    brandColor: 'dodgerblue',
+    brandColorContrast: 'white',
+    renderPages: true,
+    renderComponents: true,
+    renderJS: true,
+    renderCSS: true,
+    prerender: {
+      port: 3000,
+      path: '',
+      serveFolder: 'examples/httpdocs/',
+    },
+    onComplete: function() {
+      console.log('callback');
     }
-  </script>
-  `,
-  brandColor: 'dodgerblue',
-  brandColorContrast: 'white',
-  renderPages: true,
-  renderComponents: true,
-  renderJS: true,
-  renderCSS: true,
-  prerender: {
-    port: 3000,
-    path: '',
-    serveFolder: 'examples/httpdocs/',
-  },
-  onComplete: function() {
-    console.log('callback');
-  }
-});
+  });
+}
+
+build();
+
+setTimeout(function() {
+  dm.interrupt(build);
+}, 1500);
