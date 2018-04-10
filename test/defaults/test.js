874cca18dfaab7f1bdeea5203d526afe536297f5
Arjen <aratramba@gmail.com>
2018-04-10 16:02:07 +0200

    update test

--- test/defaults/test.js
+++ test/defaults/test.js
@@ -16,7 +16,7 @@ let config = {
 }
 
 test.cb('defaults', t => {
-  t.plan(12);
+  t.plan(8);
   rimraf.sync(__dirname + '/tmp');
 
   DM.build(Object.assign({}, config, {
@@ -39,26 +39,6 @@ test.cb('defaults', t => {
         t.true(isThere(config.output + 'lib/component1.html'), 'component1.html exists');
         t.true(isThere(config.output + 'lib/component2.html'), 'component2.html exists');
         t.true(isThere(config.output + 'lib/component3.html'), 'component3.html exists');
-
-        // test output html
-        let componentsHtmlFixture = fs.readFileSync(config.pages + 'components.html', 'utf8');
-        let componentsHtmlTmp = fs.readFileSync(config.output + 'components.html', 'utf8');
-        t.is(componentsHtmlFixture, componentsHtmlTmp);
-
-        // test libfile 1 output html
-        let components1HtmlFixture = fs.readFileSync(config.pages + 'lib/component1.html', 'utf8');
-        let components1HtmlTmp = fs.readFileSync(config.output + 'lib/component1.html', 'utf8');
-        t.is(components1HtmlFixture, components1HtmlTmp);
-
-        // test libfile 2 output html
-        let components2HtmlFixture = fs.readFileSync(config.pages + 'lib/component2.html', 'utf8');
-        let components2HtmlTmp = fs.readFileSync(config.output + 'lib/component2.html', 'utf8');
-        t.is(components2HtmlFixture, components2HtmlTmp);
-
-        // test libfile 3 output html
-        let components3HtmlFixture = fs.readFileSync(config.pages + 'lib/component3.html', 'utf8');
-        let components3HtmlTmp = fs.readFileSync(config.output + 'lib/component3.html', 'utf8');
-        t.is(components3HtmlFixture, components3HtmlTmp);
 
         t.end();
       }, 1000);
