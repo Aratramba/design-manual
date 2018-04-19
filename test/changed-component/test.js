8a995014c2e96bb30e36bfc387be7794bb913080
Arjen <aratramba@gmail.com>
2018-04-19 15:03:06 +0200

    clean up; add todo tests

---
+++ test/changed-component/test.js
@@ -0,0 +1,4 @@
+import test from 'ava';
+
+test.todo('test if changed components are detected and all are built');
+test.todo('test if no changed components are detected and short cuts components build process');
---
+++ test/changed-config/test.js
@@ -0,0 +1,3 @@
+import test from 'ava';
+
+test.todo('test if changed configuration detection works and triggers forced rebuild');
