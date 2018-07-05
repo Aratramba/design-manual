434ea61abb44419d747a029ea19868dda1a632fd
Arjen <aratramba@gmail.com>
2018-07-05 08:06:39 +0200

    add static report

--- test/autofill/wip.js
+++ test/autofill/wip.js
@@ -2,17 +2,6 @@
 // const fs = require('fs');
 const analyzer = require("../../lib/static-analysis");
 // const DM = require('../../lib/index');
-const util = require("util");
+// const util = require("util");
 
-analyzer.scan(__dirname + "/").then(results => {
-  console.log(util.inspect(results, false, null));
-
-  const usedComponents = analyzer.getScanUsedComponents(results);
-  console.log("used", usedComponents);
-
-  const unusedComponents = analyzer.getScanUnusedComponents(
-    results,
-    require("../components.json")
-  );
-  console.log("unused", unusedComponents);
-});
+analyzer.report(__dirname + "/", require("../components.json"));
