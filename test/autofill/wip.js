7c73c78edeecb4a5103dab03a92b4ac983603732
Arjen <aratramba@gmail.com>
2018-07-09 09:56:21 +0200

    add autofill tag replacement

--- test/autofill/wip.js
+++ test/autofill/wip.js
@@ -1,7 +1,28 @@
 // const rimraf = require('rimr̦af');
 // const fs = require('fs');
-const analyzer = require("../../lib/static-analysis");
+// const analyzer = require("../../lib/static-analysis");
 // const DM = require('../../lib/index');
 // const util = require("util");
 
-analyzer.report(__dirname + "/", require("../components.json"));
+// analyzer.report(__dirname + "/", require("../components.json"));
+
+const rimraf = require('rimraf');
+const fs = require('fs');
+const analyzer = require('../../lib/static-analysis');
+const DM = require('../../lib/index');
+const util = require('util')
+
+const config = {
+  output: __dirname + '/tmp/',
+  pages: __dirname + '/',
+  components: './test/components.json',
+  renderComponents: true,
+  renderCSS: false,
+  meta: {
+    domain: 'website.com',
+    title: 'Design Manual'
+  }
+}
+
+rimraf.sync(__dirname + '/tmp/');
+DM.build(config);
